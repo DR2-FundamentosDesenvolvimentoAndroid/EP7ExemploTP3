678de0064ca83676e29e80e26442d417a063c488
anathsu <willian.silva@al.infnet.edu.br>
2020-03-20 10:01:42 -0300

    Projeto criado

---
+++ app/src/main/java/com/example/ep7exemplo/CadastroManualActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ep7exemplo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CadastroManualActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_manual)
+    }
+}
